c598e3d40d07aefb025ac17fc4276b921814ccdc
ivan <ivan@mymac.lan>
2019-07-14 00:32:35 +0800

    test

--- src/com/quartztest/MyTest1.java
+++ src/com/quartztest/MyTest1.java
@@ -9,5 +9,6 @@ public class MyTest1 implements org.quartz.Job {
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		
 		System.out.println("测试1" + "   " + new Date());
+		System.out.println("test");
 	}
 }
